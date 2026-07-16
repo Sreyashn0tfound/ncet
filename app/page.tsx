5445457b358c1f51d6be344bf6025074875319db
Sreyash <minecraftsreyash@gmail.com>
2026-07-16 19:10:59 +0530

    added helpline

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,11 @@ export default function AdmissionsPortal() {
       <div className="fixed top-0 left-0 w-full bg-[#122e5c]/95 backdrop-blur-md z-[100] border-b border-white/10 px-4 md:px-8 py-3 flex justify-between items-center shadow-[0_10px_30px_rgba(0,0,0,0.5)]">
         <div className="text-white text-[10px] md:text-xs font-bold tracking-widest uppercase flex flex-col md:flex-row md:items-center gap-0.5 md:gap-2">
           <span className="opacity-70">Admissions Helpline:</span>
-          <a href="tel:97314 07062, 91643 93100" className="text-[#f97316] text-xs md:text-sm">+91 97314 07062, +91 91643 93100</a>
+          <a href="tel:7090100505
+9164895666
+9513550720" className="text-[#f97316] text-xs md:text-sm">+91 7090100505
+ +91 9164895666
+ +91 9513550720</a>
         </div>
         <button 
           onClick={scrollToLogin}
